e792ee72a406322d602b865a86203b8d6cea5f4c
Cleidimar Viana <cleidimarviana@gmail.com>
2019-02-25 02:18:03 -0300

    list itens after search in form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
 import React, { Component } from 'react';
 import './App.css';
-import { ListGroup } from 'react-bootstrap';
+import { InputGroup, FormControl, Button, ListGroup } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import alimentos from './alimentos.json';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      locationResults: [],
+      searchStr: '',
+    };
+
+
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSearch(event) {
+
+    this.setState({ searchStr: event.target.value });
+    this.state.locationResults = [];
+
+    if (this.state.searchStr.length > 1) {
+      for (let i = 0; i < alimentos.length; i++) {
+        if (alimentos[i].descricao.toLowerCase().includes(this.state.searchStr.toLowerCase())) {
+          if (!this.state.locationResults.includes(alimentos[i].descricao))
+            this.state.locationResults.push(alimentos[i].descricao);
+        }
+
+      }
+    }
+  }
+
+  clickItemResult(item){
+    this.setState({ searchStr: "" });
+    this.state.locationResults = [];
+    console.log(item);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
   render() {
+
     return (
+
       <div>
-       <ListGroup>
 
-              {
-                Object.keys(alimentos).map(function(key) {
-                  return <ListGroup.Item>{alimentos[key].descricao}</ListGroup.Item>
-                }.bind(this))
-              }
+        <div className="masthead__content">
+          <div className="hc-masthead__search">
+            <h1 style={{ textAlign: 'center' }} className="heading__primary">ALIMENTAR</h1>
+            <form onSubmit={this.handleSubmit}>
+              <InputGroup size="lg">
+                <FormControl
 
-      </ListGroup>
-          <a href="https://ww2.ibge.gov.br/home/default.php"> Dados: IBGE (Tabelas de Composição Nutricional dos Alimentos Consumidos no Brasil)</a>
+                  placeholder="Qual alimento você deseja buscar?"
+                  aria-label="Qual alimento você deseja buscar?"
+                  aria-describedby="inputGroup-sizing-sm"
+                  value={this.state.searchStr}
+                  onChange={this.handleSearch}
+                  autoFocus
+                />
+                <InputGroup.Append>
+                  <Button type="submit" variant="outline-secondary"><FontAwesomeIcon icon={faSearch} /></Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </form>
+            <ListGroup>{
+              Object.keys(alimentos).map(function (key) {
+
+                for (let i = 0; i < this.state.locationResults.length; i++) {
+                  if (this.state.locationResults[i] == alimentos[key].descricao) {
+                    var preparacao = (alimentos[key].descricao_preparacao !== 'NAO SE APLICA') ? alimentos[key].descricao_preparacao : '';
+                    return <ListGroup.Item onClick={()=>this.clickItemResult(alimentos[key])} action>{alimentos[key].descricao + " " + preparacao}</ListGroup.Item>;
+                  }
+                }
+
+              }.bind(this))
+            }
+            </ListGroup>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
